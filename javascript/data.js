f2ffe9598ad86ce310f353d818605de1eae406b6
AllenChen0958 <AllenChen0958@gmail.com>
2018-04-16 02:19:36 +0800

    version2

---
+++ javascript/data.js
@@ -0,0 +1,869 @@
+var global_videos = [
+	{
+		"id": 1,
+		"ytVideoId": "fGPPfZIvtCw",
+		"title": "功夫熊貓三 官方預告片#2",
+		"duration": "02:54",
+		"views":"125",
+		"subtitle": [
+			{
+				"start": 10.28,
+				"end": 11.5,
+				"text": "PO: Master Shifu?"
+			},
+			{
+				"start": 13.4,
+				"end": 14.86,
+				"text": "Good time? Bad time?"
+			},
+			{
+				"start": 15.52,
+				"end": 17.8,
+				"text": "SHIFU: Time is an illusion,"
+			},
+			{
+				"start": 18,
+				"end": 20.96,
+				"text": "there is only the now."
+			},
+			{
+				"start": 20.96,
+				"end": 23.4,
+				"text": "PO: So now's a... good time?"
+			},
+			{
+				"start": 23.4,
+				"end": 24.16,
+				"text": "SHIFU: Oh..."
+			},
+			{
+				"start": 25.64,
+				"end": 29.52,
+				"text": "SHIFU: You must take the next step on\nyour journey from warrior to teacher"
+			},
+			{
+				"start": 29.6,
+				"end": 30.52,
+				"text": "PO: But I'm no good at it"
+			},
+			{
+				"start": 30.7,
+				"end": 33.95,
+				"text": "PO: Oh no! It's the dragon teacher! He's so handsome!"
+			},
+			{
+				"start": 34.1,
+				"end": 35.199,
+				"text": "WA-TAIIII!"
+			},
+			{
+				"start": 37.94,
+				"end": 38.8,
+				"text": "MR. PING: Po?"
+			},
+			{
+				"start": 38.8,
+				"end": 41.099,
+				"text": "PO: Oh heyy, Dad. What's up?"
+			},
+			{
+				"start": 41.42,
+				"end": 42.38,
+				"text": "SHIFU: You're terrible at it."
+			},
+			{
+				"start": 43.62,
+				"end": 45.099,
+				"text": "PO: Who... are you?"
+			},
+			{
+				"start": 45.52,
+				"end": 47.42,
+				"text": "LI: I'm looking for my son"
+			},
+			{
+				"start": 48.76,
+				"end": 49.339,
+				"text": "PO: Dad?"
+			},
+			{
+				"start": 49.44,
+				"end": 50.68,
+				"text": "LI: Give your old man a hug!"
+			},
+			{
+				"start": 53.96,
+				"end": 56,
+				"text": "MR. PING: How do we know he's even related to you?"
+			},
+			{
+				"start": 56.02,
+				"end": 57.14,
+				"text": "LI: Look at that!"
+			},
+			{
+				"start": 61.44,
+				"end": 62.7,
+				"text": "LI: Come with me."
+			},
+			{
+				"start": 63,
+				"end": 65.68,
+				"text": "There's a secret panda village in the mountains."
+			},
+			{
+				"start": 67.08,
+				"end": 67.88,
+				"text": "PO: Whoa..."
+			},
+			{
+				"start": 67.94,
+				"end": 69.46,
+				"text": "LI: Welcome home, son"
+			},
+			{
+				"start": 70.2,
+				"end": 72.76,
+				//"text": "PO: You look just like me, but a baby."
+				"text": "我覺得這次作業好像要爆掉了"
+			},
+			{
+				"start": 72.76,
+				"end": 74.36,
+				"text": "You're like me but old"
+			},
+			{
+				"start": 74.52,
+				"end": 76.22,
+				"text": "GRANDMA PANDA: Ohh... He's adorable!"
+			},
+			{
+				"start": 77.6,
+				"end": 78.3,
+				"text": "Ow!"
+			},
+			{
+				"start": 79.7,
+				"end": 80.76,
+				"text": "I'm good!"
+			},
+			{
+				"start": 82.54,
+				"end": 84.54,
+				"text": "KAI: Kai has returned!"
+			},
+			{
+				"start": 84.9,
+				"end": 85.4,
+				"text": "Who?"
+			},
+			{
+				"start": 85.54,
+				"end": 87.6,
+				"text": "KAI: The Master of Pain. Beast of Vengeance."
+			},
+			{
+				"start": 87.6,
+				"end": 88.759,
+				"text": "Maker of Widows..."
+			},
+			{
+				"start": 88.76,
+				"end": 89.26,
+				"text": "Eh?"
+			},
+			{
+				"start": 89.26,
+				"end": 91.04,
+				"text": "KAI: Okay, I used to work with Oogway..."
+			},
+			{
+				"start": 91.04,
+				"end": 92.78,
+				"text": "Oh! Master Oogway!"
+			},
+			{
+				"start": 92.8,
+				"end": 94,
+				"text": "KAI: Silence!"
+			},
+			{
+				"start": 94,
+				"end": 96.16,
+				"text": "TIGRESS: Kai attacked the valley."
+			},
+			{
+				"start": 96.16,
+				"end": 97.08,
+				"text": "It's all gone."
+			},
+			{
+				"start": 97.44,
+				"end": 99.2,
+				"text": "Now he's coming for the pandas."
+			},
+			{
+				"start": 99.64,
+				"end": 101.72,
+				"text": "You must teach them to fight."
+			},
+			{
+				"start": 102.32,
+				"end": 103.839,
+				"text": "PO: I am ready."
+			},
+			{
+				"start": 104.72,
+				"end": 106.46,
+				"text": "I'm gonna turn you into kung fu masters"
+			},
+			{
+				"start": 106.74,
+				"end": 107.52,
+				"text": "HAI!"
+			},
+			{
+				"start": 111.48,
+				"end": 113.36,
+				"text": "Nobody said this was gonna be easy."
+			},
+			{
+				"start": 113.8,
+				"end": 115.02,
+				"text": "MEI MEI: Get ready to dance..."
+			},
+			{
+				"start": 115.12,
+				"end": 116.36,
+				"text": "...with danger!"
+			},
+			{
+				"start": 118.36,
+				"end": 119.96,
+				"text": "PO: Enemies of justice. Prepare for..."
+			},
+			{
+				"start": 119.96,
+				"end": 122.08,
+				"text": "Whoa! Are you kidding me?"
+			},
+			{
+				"start": 122.44,
+				"end": 124.64,
+				"text": "Whoa! Ah! Ow!"
+			},
+			{
+				"start": 125.8,
+				"end": 128,
+				"text": "KAI: You must be the dragon warrior."
+			},
+			{
+				"start": 128,
+				"end": 130.12,
+				"text": "PO: How bout you spare me the chit chat?"
+			},
+			{
+				"start": 130.12,
+				"end": 131.12,
+				"text": "KAI: I'm going to take your..."
+			},
+			{
+				"start": 131.12,
+				"end": 132.38,
+				"text": "PO: Ugh. Chit chat!"
+			},
+			{
+				"start": 132.38,
+				"end": 132.88,
+				"text": "KAI: In the-"
+			},
+			{
+				"start": 132.88,
+				"end": 133.82,
+				"text": "PO: Chitt chitty chat chat."
+			},
+			{
+				"start": 133.88,
+				"end": 134.38,
+				"text": "KAI: In..."
+			},
+			{
+				"start": 134.46,
+				"end": 135.42,
+				"text": "PO: Chat chat chat."
+			},
+			{
+				"start": 136.06,
+				"end": 136.56,
+				"text": "KAI: In the-"
+			},
+			{
+				"start": 136.66,
+				"end": 137.16,
+				"text": "PO: Chit chat."
+			},
+			{
+				"start": 137.92,
+				"end": 139.2,
+				"text": "PO: Yee haw!"
+			},
+			{
+				"start": 140.32,
+				"end": 141.38,
+				"text": "MANTIS: Got to get in there."
+			},
+			{
+				"start": 141.4,
+				"end": 141.96,
+				"text": "CRANE: But Master Shifu said..."
+			},
+			{
+				"start": 142.02,
+				"end": 143.26,
+				"text": "MANTIS: You're seriously afraid?"
+			},
+			{
+				"start": 143.34,
+				"end": 146.06,
+				"text": "Even Master Chicken's going in there, and he's a chicken."
+			},
+			{
+				"start": 146.2,
+				"end": 147.7,
+				"text": "MASTER CHICKEN: Bock-bock bocka!"
+			}
+		]
+	},
+	{
+		"id" : 2,
+		"ytVideoId" : "10r9ozshGVE",
+		"title": "功夫熊貓三 官方預告片#1",
+		"duration":"02:06",
+		"views":"220",
+		"subtitle": [
+		  {
+			"start": 8.78,
+			"end": 10.36,
+			"text": "NARRATOR: A Kung Fu Master"
+		  },
+		  {
+			"start": 11.88,
+			"end": 14.02,
+			"text": "NARRATOR: is prepared to train hard,"
+		  },
+		  {
+			"start": 16.08,
+			"end": 17.10,
+			"text": "NARRATOR: move fast,"
+		  },
+		  {
+			"start": 18.46,
+			"end": 19.64,
+			"text": "NARRATOR: be brave,"
+		  },
+		  {
+			"start": 23.607,
+			"end": 24.487,
+			"text": "NARRATOR: and above all,"
+		  },
+		  {
+			"start": 27.06,
+			"end": 28.44,
+			"text": "NARRATOR: be ready"
+		  },
+		  {
+			"start": 28.694,
+			"end": 29.654,
+			"text": "NARRATOR: for anything."
+		  },
+		  {
+			"start": 35.56,
+			"end": 36.36,
+			"text": "MASTER SHIFU: Perfect."
+		  },
+		  {
+			"start": 36.88,
+			"end": 37.58,
+			"text": "PO: Really?"
+		  },
+		  {
+			"start": 37.916,
+			"end": 38.416,
+			"text": "MASTER SHIFU: No."
+		  },
+		  {
+			"start": 38.758,
+			"end": 39.258,
+			"text": "MASTER SHIFU: Again!"
+		  },
+		  {
+			"start": 41.32,
+			"end": 42.66,
+			"text": "NARRATOR: But next January,"
+		  },
+		  {
+			"start": 42.86,
+			"end": 44.46,
+			"text": "NARRATOR: nothing can prepare him"
+		  },
+		  {
+			"start": 47.057,
+			"end": 49.477,
+			"text": "NARRATOR: for his greatest challenge yet."
+		  },
+		  {
+			"start": 51.56,
+			"end": 52.66,
+			"text": "LI: Yay!"
+		  },
+		  {
+			"start": 53.32,
+			"end": 55.28,
+			"text": "PO: Who are you?"
+		  },
+		  {
+			"start": 56.04,
+			"end": 57.42,
+			"text": "LI: I'm Li Shan."
+		  },
+		  {
+			"start": 57.96,
+			"end": 59.98,
+			"text": "LI: I'm looking for my son."
+		  },
+		  {
+			"start": 60.24,
+			"end": 61,
+			"text": "CROWD: [GASP]"
+		  },
+		  {
+			"start": 61.92,
+			"end": 63,
+			"text": "MR. PING: [GASP]"
+		  },
+		  {
+			"start": 63.16,
+			"end": 63.66,
+			"text": "PO: [GASP]"
+		  },
+		  {
+			"start": 64.1,
+			"end": 65.3,
+			"text": "PO: You lost your son?"
+		  },
+		  {
+			"start": 65.76,
+			"end": 66.54,
+			"text": "LI: Yes"
+		  },
+		  {
+			"start": 67.18,
+			"end": 69.62,
+			"text": "LI: Many years ago."
+		  },
+		  {
+			"start": 69.9,
+			"end": 71.02,
+			"text": "PO: I lost my father."
+		  },
+		  {
+			"start": 71.8,
+			"end": 73.54,
+			"text": "LI: I'm very sorry."
+		  },
+		  {
+			"start": 73.88,
+			"end": 74.94,
+			"text": "PO: Thank you."
+		  },
+		  {
+			"start": 77.64,
+			"end": 78.2,
+			"text": "LI: Well..."
+		  },
+		  {
+			"start": 79.16,
+			"end": 80.28,
+			"text": "LI: good luck to you."
+		  },
+		  {
+			"start": 80.92,
+			"end": 81.48,
+			"text": "PO: You too."
+		  },
+		  {
+			"start": 81.98,
+			"end": 83.58,
+			"text": "PO: I hope you find your son."
+		  },
+		  {
+			"start": 84.56,
+			"end": 86.96,
+			"text": "LI: And I hope you find your father."
+		  },
+		  {
+			"start": 94.82,
+			"end": 95.52,
+			"text": "CROWD: [GROAN]"
+		  },
+		  {
+			"start": 97.46,
+			"end": 110.42,
+			"text": "[music]"
+		  }
+		]
+	},
+	{
+		"id": 3,
+		"ytVideoId": "yqN7nHM1YTA",
+		"title": "功夫熊貓三 官方預告片#3",
+		"duration":"02:48",
+		"views":"114",
+		"subtitle": [
+		  {
+			"start": 1.68,
+			"end": 4.66,
+			"text": "SHIFU: Po. You must take the next step on your journey."
+		  },
+		  {
+			"start": 6.34,
+			"end": 8.58,
+			"text": "Incredible power awaits you."
+		  },
+		  {
+			"start": 11,
+			"end": 12.28,
+			"text": "PO: Haii! JUSTICE"
+		  },
+		  {
+			"start": 12.38,
+			"end": 14.32,
+			"text": "is about to be served."
+		  },
+		  {
+			"start": 15.96,
+			"end": 19.08,
+			"text": "PO: We'll have two justice platters please. Tigress did you want extra sauce with that?"
+		  },
+		  {
+			"start": 19.08,
+			"end": 19.84,
+			"text": "MONKEY: She wants it on the side."
+		  },
+		  {
+			"start": 19.98,
+			"end": 20.7,
+			"text": "TIGRESS: On the side."
+		  },
+		  {
+			"start": 20.7,
+			"end": 21.34,
+			"text": "PO: On the side."
+		  },
+		  {
+			"start": 22.54,
+			"end": 26.6,
+			"text": "SHIFU: If you only do what you can do, you will never be more than you are now."
+		  },
+		  {
+			"start": 27.48,
+			"end": 28.76,
+			"text": "PO: I like who I am!"
+		  },
+		  {
+			"start": 28.98,
+			"end": 31.54,
+			"text": "SHIFU: You don't even know who you are."
+		  },
+		  {
+			"start": 33.04,
+			"end": 35.08,
+			"text": "LI: I'm looking for my son."
+		  },
+		  {
+			"start": 35.7,
+			"end": 36.52,
+			"text": "PO: Dad??"
+		  },
+		  {
+			"start": 36.58,
+			"end": 38.02,
+			"text": "LI: Give your old man a hug!"
+		  },
+		  {
+			"start": 39.74,
+			"end": 41.9,
+			"text": "MR. PING: How do we know he's even related to you?"
+		  },
+		  {
+			"start": 41.92,
+			"end": 45.6,
+			"text": "LI: Look at that! It's like looking in a fat mirror."
+		  },
+		  {
+			"start": 46.58,
+			"end": 47.68,
+			"text": "LI: Come home with me."
+		  },
+		  {
+			"start": 47.82,
+			"end": 48.62,
+			"text": "PO: Whoa..."
+		  },
+		  {
+			"start": 48.62,
+			"end": 49.94,
+			"text": "MEI MEI: Hi, I'm Mei Mei."
+		  },
+		  {
+			"start": 50.98,
+			"end": 54.58,
+			"text": "LI: You must rediscover what it is to be a panda."
+		  },
+		  {
+			"start": 59.74,
+			"end": 60.28,
+			"text": "LI: There, There."
+		  },
+		  {
+			"start": 60.28,
+			"end": 61.18,
+			"text": "PO: I'm fine, I'm fine"
+		  },
+		  {
+			"start": 62.42,
+			"end": 63.04,
+			"text": "LI: There you go."
+		  },
+		  {
+			"start": 65.98,
+			"end": 68.18,
+			"text": "KAI: Kai has returned!"
+		  },
+		  {
+			"start": 68.64,
+			"end": 69.32,
+			"text": "FARMERS: Who?"
+		  },
+		  {
+			"start": 69.42,
+			"end": 70.88,
+			"text": "KAI: The master of pain. Beast of vengeance."
+		  },
+		  {
+			"start": 70.9,
+			"end": 72.18,
+			"text": "Maker of widows..."
+		  },
+		  {
+			"start": 72.18,
+			"end": 74.26,
+			"text": "Okay, I used to work with Oogway"
+		  },
+		  {
+			"start": 74.7,
+			"end": 76.08,
+			"text": "FARMERS: Oh, Master Oogway!"
+		  },
+		  {
+			"start": 76.22,
+			"end": 76.9,
+			"text": "KAI: Silence!"
+		  },
+		  {
+			"start": 78.86,
+			"end": 80.7,
+			"text": "OOGWAY: You must save the world."
+		  },
+		  {
+			"start": 81.52,
+			"end": 82.66,
+			"text": "PO: There's no way I can stop him!"
+		  },
+		  {
+			"start": 82.9,
+			"end": 84.66,
+			"text": "LI: Unless you had an army of your own."
+		  },
+		  {
+			"start": 84.66,
+			"end": 86.48,
+			"text": "PO: You don't even know kung fu."
+		  },
+		  {
+			"start": 86.5,
+			"end": 87.94,
+			"text": "LI: Then you will teach us."
+		  },
+		  {
+			"start": 91.86,
+			"end": 93.56,
+			"text": "PO: Nobody said this was gonna be easy."
+		  },
+		  {
+			"start": 94.52,
+			"end": 96.48,
+			"text": "PO: I'm gonna turn you into kung fu masters."
+		  },
+		  {
+			"start": 97.08,
+			"end": 97.9,
+			"text": "PO: My tenders!"
+		  },
+		  {
+			"start": 99.22,
+			"end": 101.4,
+			"text": "PO: The dumpling squadron will take position here"
+		  },
+		  {
+			"start": 101.52,
+			"end": 102.4,
+			"text": "on my signal."
+		  },
+		  {
+			"start": 104.58,
+			"end": 105.82,
+			"text": "PO: The noodle squadron will..."
+		  },
+		  {
+			"start": 108.86,
+			"end": 110.3,
+			"text": "This is the spot where..."
+		  },
+		  {
+			"start": 111.64,
+			"end": 113.24,
+			"text": "Okay, I saw that coming."
+		  },
+		  {
+			"start": 114.6,
+			"end": 117.04,
+			"text": "KAI: This was the one destined to stop me?"
+		  },
+		  {
+			"start": 117.04,
+			"end": 118.6,
+			"text": "PO: How bout you spare me the chitchat?"
+		  },
+		  {
+			"start": 118.7,
+			"end": 119.74,
+			"text": "KAI: I'm going to take your-"
+		  },
+		  {
+			"start": 119.74,
+			"end": 120.48,
+			"text": "PO: Chit chat."
+		  },
+		  {
+			"start": 120.54,
+			"end": 121.04,
+			"text": "KAI: In the-"
+		  },
+		  {
+			"start": 121.12,
+			"end": 123.28,
+			"text": "PO: Chitty chitty chat chat chat chat chat."
+		  },
+		  {
+			"start": 124.2,
+			"end": 124.7,
+			"text": "KAI: In the-"
+		  },
+		  {
+			"start": 124.76,
+			"end": 125.26,
+			"text": "PO: Chit chat."
+		  },
+		  {
+			"start": 130.98,
+			"end": 132.82,
+			"text": "PO: This is Master Flying Rhino's battle armor!"
+		  },
+		  {
+			"start": 132.82,
+			"end": 134.74,
+			"text": "LI: Wonder what this does. I should pull it."
+		  },
+		  {
+			"start": 142.54,
+			"end": 143.82,
+			"text": "PO: I think I just peed a little."
+		  }
+		]
+	},
+	{
+		"id": 4,
+		"ytVideoId": "c-lwDdLKz9k",
+		"title": "【七大罪】劇場版預告 ! 決戰最兇惡魔神!!",
+		"duration":"00:52",
+		"views":"185",
+		"subtitle": [
+		  {
+			"start": 3.18,
+			"end": 4.26,
+			"text": "這是一個"
+		  },
+		  {
+			"start": 4.26,
+			"end": 8.1,
+			"text": "人類與非人類的世界尚未分離之時的"
+		  },
+		  {
+			"start": 8.1,
+			"end": 11.2,
+			"text": "古老傳說"
+		  },
+		  {
+			"start": 11.2,
+			"end": 15.04,
+			"text": "守護國家的聖騎士們擁有強大的魔力"
+		  },
+		  {
+			"start": 15.04,
+			"end": 19.8,
+			"text": "在此同時 其中最可怕且令人敬畏的是"
+		  },
+		  {
+			"start": 19.8,
+			"end": 23.18,
+			"text": "聖騎士中名聲最響的"
+		  },
+		  {
+			"start": 23.18,
+			"end": 25.24,
+			"text": "人們稱他們為"
+		  },
+		  {
+			"start": 25.24,
+			"end": 28.14,
+			"text": "七大罪"
+		  },
+		  {
+			"start": 29.78,
+			"end": 33.96,
+			"text": "復活的魔神族團隊 黑色六騎士中最殘酷的"
+		  },
+		  {
+			"start": 33.96,
+			"end": 35.38,
+			"text": "貝瑞利翁"
+		  },
+		  {
+			"start": 37.58,
+			"end": 40.58,
+			"text": "將與七大罪展開衝突!!!"
+		  },
+		  {
+			"start": 43.7,
+			"end": 48.04,
+			"text": "七大罪劇場版 天空囚人"
+		  },
+		  {
+			"start": 48.04,
+			"end": 50.52,
+			"text": "8月18日將在日本全國上映"
+		  }
+		]
+	}
+];
---
+++ javascript/main_page.js
@@ -0,0 +1,230 @@
+// var video_info = [
+//     {"id":111111, "video-time":"03:16", "title":"Kung Fu Panda 1 | Official Trailer #1", "src":"video_page.html?id=1", ""},
+//     {"id":111112, "video-time":"06:06", "title":"Kung Fu Panda 3 | Official Trailer #2", "src":"video_page.html?id=2", ""},
+//     {"id":111113, "video-time":"06:06", "title":"Kung Fu Panda 3 | Official Trailer #4"}
+// ]
+
+
+// for(var i=0 ; i<video_info.length ; i++){
+//     var video_i = document.getElementById(video_info[i]["id"]);
+//     console.log(video_i.childNodes[3].childNodes[1]["href"]);
+//     if(video_i!==null){
+//         video_i.childNodes[1].childNodes[1]["href"]=video_info[i]["src"];
+//         video_i.childNodes[1].childNodes[3].childNodes[1].innerHTML=video_info[i]["video-time"];
+//         video_i.childNodes[3].childNodes[1]["href"]=video_info[i]["src"];
+//         video_i.childNodes[3].childNodes[1].childNodes[1].innerHTML=video_info[i]["title"];
+//         // console.log(video_i.childNodes[3].childNodes[1].childNodes[1].innerHTML)
+//     }
+// }
+
+// for(var i=0 ; i<global_videos.length ; i++){
+//     var video_i = document.getElementById(global_videos[i]["id"]);
+//     console.log(video_i.childNodes[3].childNodes[1]["href"]);
+//     if(video_i!==null){
+//         video_i.childNodes[1].childNodes[1]["href"]="https://www.youtube.com/watch?v="+global_videos[i]["ytVideoId"];
+//         // video_i.childNodes[1].childNodes[3].childNodes[1].innerHTML=global_videos[i]["video-time"];
+//         video_i.childNodes[3].childNodes[1]["href"]="https://www.youtube.com/watch?v="+global_videos[i]["ytVideoId"];
+//         video_i.childNodes[3].childNodes[1].childNodes[1].innerHTML=global_videos[i]["title"];
+        
+//         // console.log(video_i.childNodes[3].childNodes[1].childNodes[1].innerHTML)
+//     }
+// }
+
+// (function (){
+//     var tag = document.createElement("script"),
+//         firstScriptTag = document.getElementsByTagName("script")[0];
+
+//         tag.src = "https://www.youtube.com/iframe_api";
+//         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// })();
+
+//============================================================
+// var player;
+// function onYouTubeIframeAPIReady() {
+//   player = new YT.Player('player', {
+//     height: '390',
+//     width: '640',
+//     videoId: 'M7lc1UVf-VE',
+//     events: {
+//       'onReady': onPlayerReady,
+//       'onStateChange': onPlayerStateChange
+//     }
+//   });
+//   // var dur = !player.getDuration? 0 : player.getDuration();
+//   // console.log(dur);
+//   // player.cueVideoById('bHQqvYy5KYo', 5, 60, "small")
+  
+// }
+
+// var getVideoTime = function (player) {
+//     if(player!==undefined){
+//         if(player.getDuration()!==undefined){
+//             player.pauseVideo();
+//             player.unMute();
+//             console.log(player.getDuration());
+//         }
+//         else{
+//             setTimeout(getVideoTime, 150);
+//             console.log("call again ");
+//         }
+       
+//     }else{
+//         setTimeout(getVideoTime, 150);
+//         console.log("call again ");
+//     }
+// }
+
+
+// function onPlayerReady(event) {
+//     event.target.playVideo();
+//     console.log(player.getDuration());
+//     player.cueVideoById('bHQqvYy5KYo', 5, 60, "small");
+//     getVideoTime(player);    
+// }
+
+// var done = false;
+// function onPlayerStateChange(event) {
+//     if (event.data == YT.PlayerState.PLAYING && !done) {
+//         setTimeout(stopVideo, 6000);
+//         done = true;
+//     }
+// }
+// function stopVideo() {
+//     player.stopVideo();
+// }
+// This function creates an <iframe> (and YouTube player)
+// after the API code downloads.
+
+// function onYouTubeIframeAPIReady() {
+//     var player = new YT.Player('player', {
+//         width: '100%',
+//         // events: {
+//         //   'onReady': onPlayerReady,
+//         //   'onStateChange': onPlayerStateChange
+//         // }
+//         // videoId: currentVideo.ytVideoId,
+//     });
+//     player.cueVideoById("fGPPfZIvtCw", 0, "small");
+
+//     // var left_secssion = document.getElementById("left-section");
+//     // for(var i=0 ; i<global_videos.length ; i++) {
+//     //     var video_block = document.createElement("li");
+//     //     video_block.classList.add("span2");
+//     //     // video_block.innerHTML = '<div class="thumbnail" id='+ String(global_videos["id"]) +'>';
+//     //     // video_block.innerHTML +=     '<div class="photo">'
+//     //     // video_block.innerHTML +=     '<a href=video_page.html?id='+ String(global_videos["id"])  +'>';
+//     //     var thumbnail = document.createElement("div");
+//     //     thumbnail.classList.add("thumbnails");
+//     //     thumbnail.id = String(global_videos[i]["id"]);
+
+//     //     var cover_photo_block = document.createElement("div");
+//     //     cover_photo_block.classList.add("photo");
+
+//     //     var a = document.createElement("a");
+//     //     a.href= "video_page.html?id=" + String(global_videos[i]["id"]);
+
+//     //     var img=document.createElement("img");
+//     //     img.src = "https://i.ytimg.com/vi/" + String(global_videos[i]["ytVideoId"]) + "/hqdefault.jpg";
+
+//     //     var video_label = document.createElement("span");
+//     //     video_label.classList.add("label");
+//     //     video_label.classList.add("label-inverse");
+//     //     video_label.classList.add("class-photo-label");
+
+//     //     var video_time = document.createElement("span");
+//     //     video_time.classList.add("video-time");
+//     //     // video_time.innerHTML=
+
+//     //     player.cueVideoById(String(global_videos[i]["ytVideoId"]));
+//     //     // console.log();
+
+//     //     a.appendChild(img);
+//     //     cover_photo_block.appendChild(a)
+//     //     thumbnail.appendChild(cover_photo_block);
+//     //     video_block.appendChild(thumbnail);
+//     //     console.log(video_block);
+
+//     // }
+// };
+
+var thumbnails = document.getElementById("video_thumbnails");
+for(var i=0 ; i<global_videos.length ; i++) {
+    var video_block = document.createElement("li");
+    video_block.classList.add("span2");
+    // video_block.innerHTML = '<div class="thumbnail" id='+ String(global_videos["id"]) +'>';
+    // video_block.innerHTML +=     '<div class="photo">'
+    // video_block.innerHTML +=     '<a href=video_page.html?id='+ String(global_videos["id"])  +'>';
+    var thumbnail = document.createElement("div");
+    thumbnail.classList.add("thumbnail");
+    thumbnail.id = String(global_videos[i]["id"]);
+
+    var cover_photo_block = document.createElement("div");
+    cover_photo_block.classList.add("photo");
+
+    var a = document.createElement("a");
+    a.href= "video_page.html?id=" + String(global_videos[i]["id"]);
+
+    var img=document.createElement("img");
+    img.src = "https://i.ytimg.com/vi/" + String(global_videos[i]["ytVideoId"]) + "/hqdefault.jpg";
+
+    var video_label = document.createElement("span");
+    video_label.classList.add("label");
+    video_label.classList.add("label-inverse");
+    video_label.classList.add("class-photo-label");
+
+    var video_time = document.createElement("span");
+    video_time.classList.add("video-time");
+    video_time.innerHTML=global_videos[i]["duration"];
+    
+
+
+    a.appendChild(img);
+    cover_photo_block.appendChild(a)
+
+    video_label.appendChild(video_time);
+    cover_photo_block.appendChild(video_label);
+    thumbnail.appendChild(cover_photo_block);
+    
+    //===============================================
+    var info_block = document.createElement("div");
+    info_block.classList.add("caption");
+
+    var a = document.createElement("a");
+    a.herf="video_page.html?id=" + String(global_videos[i]["id"]);
+
+    var h5 = document.createElement("h5");
+    h5.innerHTML = global_videos[i]["title"];
+
+    var icon_div = document.createElement("div");
+    var icon = document.createElement("i");
+    icon.classList.add("icon-headphones");
+    icon.innerHTML=global_videos[i]["views"];
+    
+    var lang_label_block = document.createElement("div");
+    var span_ch = document.createElement("span");
+    var span_en = document.createElement("span");
+    span_en.classList.add("label");
+    span_en.classList.add("label-warning");
+    span_ch.classList.add("label");
+    span_en.innerHTML="美國腔";
+    span_ch.innerHTML="中文";
+
+
+    a.appendChild(h5);
+    icon_div.appendChild(icon);
+
+    lang_label_block.appendChild(span_ch);
+    lang_label_block.appendChild(span_en);
+
+    info_block.appendChild(a);
+    info_block.appendChild(icon_div);
+    info_block.appendChild(lang_label_block);
+
+
+    thumbnail.appendChild(info_block);
+    video_block.appendChild(thumbnail);
+    // console.log(video_block);
+    // console.log(thumbnails);
+    thumbnails.appendChild(video_block);
+
+}
